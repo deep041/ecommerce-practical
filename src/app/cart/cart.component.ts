f33ce2bff0afcddedad0d76f461eda2aae875fcd
Deep Patel <patel.deep041@gmail.com>
2023-09-06 18:24:30 +0530

    Cart changes

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -16,7 +16,29 @@ export class CartComponent implements OnInit {
         if (cartItems) {
             this.cart = JSON.parse(cartItems);
         }
+    }
 
+    changeQuantity(type: string, id: string): void {
+        let selectedItem = this.cart.filter((d: any) => d.medicine_id === id);
+        if (type === 'increase') {
+            selectedItem[0].quantity++;
+        } else {
+            selectedItem[0].quantity--;
+        }
+        let cart: any = localStorage.getItem('cart');
+        if (cart) {
+            cart = JSON.parse(cart);
+            if (cart.length > 0) {
+                let cartItem = cart.filter((cartItem: any) => cartItem.medicine_id === id);
+                if (selectedItem[0].quantity > 0) {
+                    cartItem[0].quantity = selectedItem[0].quantity;
+                } else {
+                    selectedItem[0].added_to_cart = false;
+                    cart.splice(cart.findIndex((a: any) => a.medicine_id === id), 1);
+                }
+                localStorage.setItem('cart', JSON.stringify(cart));
+            }
+        }
     }
 
 }
